cd9f6f1061081b4ec685897886f4584933542f34
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-09-22 14:39:50 -0400

    added in router

---
+++ src/components/spinner-section/spinner-section.js
@@ -0,0 +1,9 @@
+const spinnerSection = () => {
+    return(
+        <h1 className="text-3xl font-bold underline">
+            Spinner Section
+        </h1>
+    )
+}
+
+export default spinnerSection
---
+++ src/home.js
@@ -0,0 +1,15 @@
+import spinnerSection from './components/spinner-section/spinner-section';
+
+const Home = () => {
+    return(
+        <div className='app'>
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <spinnerSection/>
+  
+      </div>
+    )
+}
+
+export default Home
