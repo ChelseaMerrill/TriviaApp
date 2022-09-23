8acd79b0eb1116b05d043f979c56e57d6d423dfa
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-09-23 15:05:46 -0400

    begininng of wheel

--- src/components/spinner-section/spinner.js
+++ src/components/spinner-section/spinner.js
@@ -1,7 +1,23 @@
+import './spinner-section.css'; 
+
 const Spinner = () => {
+    let colors={
+
+    }
     return(
     <div className='spinner-section'>
         <h1 className="text-3xl font-bold underline">Spinner</h1>
+        <div className='main-box'>
+            <div className='box'>
+                <div className='box1'>
+                    <span className='span1'></span>
+                    <span className='span2'></span>
+                    <span className='span3'></span>
+                    <span className='span4'></span>
+                    <span className='span5'></span>
+                </div>
+            </div>
+        </div>
     </div>
     );
 }
