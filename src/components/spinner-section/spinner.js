80aa22717408647011c3ebc2013b6b817739b239
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-09-26 10:31:25 -0400

    color chart works

--- src/components/spinner-section/spinner.js
+++ src/components/spinner-section/spinner.js
@@ -11,10 +11,9 @@ const Spinner = () => {
             <div className='box'>
                 <div className='box1'>
                     <span className='span1'></span>
-                    <span className='span2'></span>
-                    <span className='span3'></span>
-                    <span className='span4'></span>
-                    <span className='span5'></span>
+                    <span className='span2 rotate-90'></span>
+                    <span className='span3 rotate-180' ></span>
+                    <span className='span4 -rotate-90'></span>
                 </div>
             </div>
         </div>
