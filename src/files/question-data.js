012391a6960d7e1ec54a57db73a467d6fe340951
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-09-27 14:39:50 -0400

    added dummy data

---
+++ src/files/question-data.js
@@ -0,0 +1,179 @@
+let QuestionsArray = [
+    {
+        id: 1,
+        category: 'Geography',
+        question: 'What is the captial of Illinois?',
+        answer: 'Springfield',
+        choices: [
+            'Chicago',
+            'Peoria',
+            'Rockford'
+        ]
+    },
+    {
+        id: 2,
+        category: 'Geography',
+        question: 'What is the captial of Florida?',
+        answer: 'Tallahasse',
+        choices: [
+            'Orlando',
+            'Tampa',
+            'Miami'
+        ]
+    },
+    {
+        id: 3,
+        category: 'Geography',
+        question: 'Which Continent is in all four hemispheres?',
+        answer: 'Africa',
+        choices: [
+            'Europe',
+            'N. America',
+            'Australia'
+        ]
+    },
+    {
+        id: 4,
+        category: 'Geography',
+        question: 'How many large islands make up Hawaii?',
+        answer: 'Eight',
+        choices: [
+            'Four',
+            'Five',
+            'Ten'
+        ]
+    },
+    {
+        id: 5,
+        category: 'History',
+        question: 'In what year did JFK die?',
+        answer: '1963',
+        choices: [
+            '1969',
+            '1964',
+            '1970'
+        ]
+    },
+    {
+        id: 6,
+        category: 'History',
+        question: 'How long was William Harrision President?',
+        answer: '32 days',
+        choices: [
+            '4 years',
+            '8 years',
+            '7 months'
+        ]
+    },
+    {
+        id: 7,
+        category: 'History',
+        question: 'Which of these states did not seceded to form the original Confederacy during the U.S, Civil War?',
+        answer: 'Kentucky',
+        choices: [
+            'Florida',
+            'Texas',
+            'South Carolina'
+        ]
+    },
+    {
+        id: 8,
+        category: 'History',
+        question: 'The ancient city of Rome was built on how many hills?',
+        answer: 'Seven',
+        choices: [
+            'Six',
+            'Five',
+            'Four'
+        ]
+    },
+    {
+        id: 9,
+        category: 'Entertainment',
+        question: 'What are the dying words of Charles Foster Kane in Citizen Kane?',
+        answer: 'Rosebud',
+        choices: [
+            'Goodbye',
+            "I'll miss you",
+            'Xanadu'
+        ]
+    },
+    {
+        id: 10,
+        category: 'Entertainment',
+        question: 'For what movie did Tom Hanks score his first Academy Award nomination?',
+        answer: 'Big',
+        choices: [
+            'Toy Story',
+            'Forest Gump',
+            'The Green Mile'
+        ]
+    },
+    {
+        id: 11,
+        category: 'Entertainment',
+        question: 'In Risky Business, what song did Tom Cruise famously lip-sync to in his underwear?',
+        answer: '"Old Time Rock and Roll" by Bob Seger',
+        choices: [
+            '"You Shook Me All Night Long" by AC/DC',
+            '"Whip It" by Devo',
+            '"Mr. Roboto" by Styx'
+        ]
+    },
+    {
+        id: 12,
+        category: 'Entertainment',
+        question: 'Alfred Hitchcock famously directed a 1963 horror movie about what type of animal?',
+        answer: 'Birds',
+        choices: [
+            'Wolves',
+            'Cicadas',
+            'Cats'
+        ]
+    },
+    {
+        id: 13,
+        category: 'Science',
+        question: 'What is the most abundant gas in the atmosphere?',
+        answer: 'Nitrogen',
+        choices: [
+            'Oxygen',
+            'Hydrogen',
+            'Carbon'
+        ]
+    },
+    {
+        id: 14,
+        category: 'Science',
+        question: 'What is the hottest planet in the solar system?',
+        answer: 'Venus',
+        choices: [
+            'Mars',
+            'Jupiter',
+            'Mercury'
+        ]
+    },
+    {
+        id: 15,
+        category: 'Science',
+        question: "What’s the boiling point of water?",
+        answer: '100 degrees C',
+        choices: [
+            '100 degrees F',
+            '32 degrees C',
+            '250 degrees F'
+        ]
+    },
+    {
+        id: 16,
+        category: 'Science',
+        question: 'How long does a human red blood cell survive?',
+        answer: '120 days',
+        choices: [
+            '1 day',
+            '60 days',
+            '280 days'
+        ]
+    },
+
+]
