d4c453796ab97e4e6c136a0ebb8acf4375f4ccf2
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-09-27 11:43:22 -0400

    added placeholder stats

--- src/components/scores-section/scores-section.js
+++ src/components/scores-section/scores-section.js
@@ -1,7 +1,12 @@
 const Section3 = () => {
     return(
     <div className='spinner-section'>
-        <h1 className="text-3xl font-bold underline">Scores Section</h1>
+        <h1 className="text-3xl font-bold underline">Top Scores</h1>
+        <div>
+            <div className='columns-2'><h1>Amanda</h1><h1>52 seconds</h1></div>
+            <div className='columns-2'><h1>Nadia</h1><h1>53 seconds</h1></div>
+            <div className='columns-2'><h1>Chelsea</h1><h1>54 seconds</h1></div>
+        </div>
     </div>
     );
 }
