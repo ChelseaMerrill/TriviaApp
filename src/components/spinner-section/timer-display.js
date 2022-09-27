5b54795f98a3d30072e86eeda3b6f2e48ab2bc3c
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-09-27 11:37:41 -0400

    timer works

--- src/components/spinner-section/timer-display.js
+++ src/components/spinner-section/timer-display.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const TimerDisplay = () => {
+class TimerDisplay extends React.Component{
+  render(){
     return (
       <div className='timer-display h-48 bg-orange-400'>
-        <h1>timer-display</h1>
-        {/* <span>
+        <span>
           {this.props.formatTime(this.props.currentTimeMin)}:
           {this.props.formatTime(this.props.currentTimeSec)}:
           {this.props.formatTime(this.props.currentTimeMs, 'ms')}
-        </span> */}
+        </span>
       </div>
     );
   }
+}
 
 export default TimerDisplay;
