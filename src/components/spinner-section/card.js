4a4ec8fe8249dc352fd109d6af4c03ce452d77f6
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-09-28 11:37:54 -0400

    fix card style

--- src/components/spinner-section/card.js
+++ src/components/spinner-section/card.js
@@ -1,8 +1,8 @@
 const Card = () => {
     return (
-        <div className='max-w-sm w-5/6 lg:max-w-full lg:flex m-8'>
+        <div className='max-w-sm w-5/6 lg:max-w-full lg:flex m-8 border border-gray-400'>
             <div className="bg-yellow-300 w-32"></div>
-            <div className=" w-96 border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div className=" w-96 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                 <div className="mb-8">
                     <div className="text-gray-600 font-bold text-xl mb-2">Trivia Question</div>
                     <ul className="text-gray-500 text-base">
